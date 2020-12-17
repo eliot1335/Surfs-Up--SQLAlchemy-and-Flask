a7d18c4c76ce75ebc13087b2fade79e5bf983f39
Eliot Chern <eliotchern@gmail.com>
2020-12-17 11:08:39 -0800

    initial setup for app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("test")
+import numpy as np
+import datetime as dt
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
